c17dcc2617999f38a1511ba696080001f3a3f723
ddanelynn <dingyoujia0823@gmail.com>
2022-01-07 19:39:38 +0800

    remove unnecesary imports

--- SecretSanta/mern-secret-santa/src/components/BirthdayEventCreationPage.js
+++ SecretSanta/mern-secret-santa/src/components/BirthdayEventCreationPage.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import birthdayImage from '../assets/birthday.png'
-import christmasImage from '../assets/christmas.png'
 import "./BirthdayEventCreation.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
