deba8f61bc6f9e715f0b7e962f33e13ed279d5e6
Jerry Ho <itsjerryho@gmail.com>
2022-01-07 20:23:38 +0800

    WIP: Add Secret Santa Page

--- SecretSanta/mern-secret-santa/src/App.js
+++ SecretSanta/mern-secret-santa/src/App.js
@@ -7,9 +7,11 @@ import { SignUpPage } from "./components/SignUpPage";
 import { HomePage } from "./components/HomePage";
 import { EventCreationPage } from "./components/EventCreationPage";
 import { BirthdayEventCreationPage } from "./components/BirthdayEventCreationPage";
+import { SecretSantaEventCreationPage } from "./components/SecretSantaEventCreationPage";
 import ProfilePage from "./components/ProfilePage";
 import reducers from "./constants/Reducers";
 import WishlistPage from "./components/WishlistPage";
+// import PrivateRoute from "./components/PrivateRoute";
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -20,8 +22,8 @@ function App() {
       <Router>
           {/* <Navbar /> */}
           <Routes>
-          <Route path="/" exact element={<BirthdayEventCreationPage/>} />
-          {/* <Route path="/" exact element={<SignInPage/>} /> */}
+          <Route path="/create/secret-santa" exact element={<SecretSantaEventCreationPage/>} />
+          <Route path="/" exact element={<SignInPage/>} />
           <Route path="/events" exact element={<EventCreationPage/>} />
           <Route path="/sign-up" exact element={<SignUpPage/>} />
           <Route path="/home" exact element={<HomePage/>} />
