b1e185c3b8d2152279cdd509fbc3a684bbd752b7
ddanelynn <dingyoujia0823@gmail.com>
2022-01-07 14:53:14 +0800

    complete main event creation page

---
+++ SecretSanta/mern-secret-santa/src/components/EventCreationPage.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import birthdayImage from '../assets/birthday.png'
+import christmasImage from '../assets/christmas.png'
+import "./EventCreation.css";
+
+export const EventCreationPage = () => {
+    const navigate = useNavigate();
+    const goToSecretSanta = () => {
+        navigate('/home')
+    }
+    const goToBirthday = () => {
+        navigate('/sign-up')
+    }
+
+    return (
+        <div className="event-creation-container">
+            <div className="event-block">
+                <div className="event-bubble" onClick={goToBirthday}>
+                    <img className="event-image" src={christmasImage}/>
+                </div>
+                <div className="event-title">Secret Santa</div>
+            </div>
+            <div className="event-block">
+                <div className="event-bubble" onClick={goToSecretSanta}>
+                    <img className="event-image" src={birthdayImage}/>
+                </div>
+                <div className="event-title">Birthday</div>
+            </div>
+        </div>
+    )
+}
